046541c5afde19f478ca87a78f5dd087bc64ebe4
ShakhzodbekYuldoshov <shahzodyoldoshov10@gmail.com>
2022-03-13 00:58:52 +0500

    incorrect labels(0, www) removed

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,23 @@
 from os import listdir
 from xml.etree import ElementTree
 
+def remove_incorrect_labels(filename):
+    # load and parse the file
+    tree = ElementTree.parse(filename)
+    # get the root of the document
+    root = tree.getroot()
+    # extract each bounding box
+    boxes = list()
+    for object in root.findall('.//name/..'):
+        name = object.find('name')
+        if name.text not in ['1', '2', '4', '5']:
+            root.remove(object)
+            print(name.text)
+    
+    # save xml  file
+    tree.write(filename)
+
+
 
 def merge_4_types(filename):
     # load and parse the file
@@ -26,10 +43,7 @@ def remove_spaces(filename):
     boxes = list()
     for name in root.findall('.//name'):
         if name.text not in ['1', '2', '4', '5']:
-            print(f'"{name.text}"')
             new_name = name.text.replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '')
-            print(filename)
-            print(f'"{new_name}"')
             name.text = new_name
     
     # save xml file
@@ -42,3 +56,4 @@ annots_names = listdir(annots_dir)
 for annot_name in annots_names:
     remove_spaces(annots_dir + annot_name)
     merge_4_types(annots_dir + annot_name)
+    remove_incorrect_labels(annots_dir + annot_name)
