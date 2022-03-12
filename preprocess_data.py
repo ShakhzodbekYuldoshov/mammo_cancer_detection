4c6d48a9e4b5c0e402501c68022fbca00e335fb3
ShakhzodbekYuldoshov <shahzodyoldoshov10@gmail.com>
2022-03-13 00:34:25 +0500

    white spaces removed around label

--- preprocess_data.py
+++ preprocess_data.py
@@ -2,22 +2,27 @@ from os import listdir
 from xml.etree import ElementTree
 
 
-def extract_boxes(filename):
-        # load and parse the file
-        tree = ElementTree.parse(filename)
-        # get the root of the document
-        root = tree.getroot()
-        # extract each bounding box
-        boxes = list()
-        for name in root.findall('.//name'):
-            name = name.text.replace(' ', '')
-            if name not in ['1', '2', '4', '5']:
-                print(filename)
-                print('"', name, '"')
+def remove_spaces(filename):
+    # load and parse the file
+    tree = ElementTree.parse(filename)
+    # get the root of the document
+    root = tree.getroot()
+    # extract each bounding box
+    boxes = list()
+    for name in root.findall('.//name'):
+        if name.text not in ['1', '2', '4', '5']:
+            print(f'"{name.text}"')
+            new_name = name.text.replace(' ', '').replace(' ', '').replace(' ', '').replace(' ', '')
+            print(filename)
+            print(f'"{new_name}"')
+            name.text = new_name
+    
+    # save xml file
+    tree.write(filename)
 
 
 annots_dir = './dataset/annots/'
 annots_names = listdir(annots_dir)
 
 for annot_name in annots_names:
-    extract_boxes(annots_dir + annot_name)
+    remove_spaces(annots_dir + annot_name)
